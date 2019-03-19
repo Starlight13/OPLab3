f2374c5738cb90a3e8cc656b5a7b0f7d469387e3
dsadas asdasd <olgarom@macbook-pro.local>
2019-03-19 16:21:05 +0200

    postfix func

--- OPLab3/main.cpp
+++ OPLab3/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "Header.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(){
+    int len = 7;
+    queue q;
+    stack op;
+    char *array = new char[len];
+    array[0] = '1';
+    array[1] = '+';
+    array[2] = '2';
+    array[3] = '*';
+    array[4] = '4';
+    array[5] = '+';
+    array[6] = '1';
+    postfix(array, len, q, op);
 }
